2c810a49d7ffc7d32e0cb9ecb4e5f1b31c759efd
ITurres <emanueliturres.co@gmail.com>
2023-11-07 19:32:10 -0300

    Feat: Enhance 'Person' class with a decorator
    - Include 'Nameable' class as an interface, which defines
      the 'correct_name' method.
    - In the 'Person' class, call super in the constructor
      to ensure proper initialization.
    - Implement the 'correct_name' method to meet the
      requirements of the 'Nameable' class interface.

--- lib/person.rb
+++ lib/person.rb
@@ -1,16 +1,23 @@
-class Person
+require_relative './class_decorators/nameable'
+
+class Person < Nameable
   attr_reader :id
   attr_accessor :name, :age
 
-  def initialize(age, name = 'Unknown', paren_permission: true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    super() #* call Nameable's initialize
     @id = Random.rand(1..1000)
     @age = age
     @name = name
-    @paren_permission = paren_permission
+    @parent_permission = parent_permission
   end
 
   def can_use_services?
-    of_age? || @paren_permission
+    of_age? || @parent_permission
+  end
+
+  def correct_name
+    @name
   end
 
   private
