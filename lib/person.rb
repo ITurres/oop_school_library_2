e0d2046d78f5a644949fb860615f6b81a2006775
ITurres <emanueliturres.co@gmail.com>
2023-11-08 21:41:02 -0300

    Feat: Add 'many-to-many' association between 'Person' and 'Book'
    - Use 'Rental' as join (intermediate) table.

--- lib/person.rb
+++ lib/person.rb
@@ -1,7 +1,7 @@
 require_relative 'class_decorators/nameable'
 
 class Person < Nameable
-  attr_reader :id
+  attr_reader :id, :rentals
   attr_accessor :name, :age
 
   def initialize(age, name = 'Unknown', parent_permission: true)
@@ -10,6 +10,7 @@ class Person < Nameable
     @age = age
     @name = name
     @parent_permission = parent_permission
+    @rentals = []
   end
 
   def can_use_services?
@@ -18,6 +19,11 @@ class Person < Nameable
 
   def correct_name
     @name
+  end
+
+  def add_rental(date, book)
+    new_rental = Rental.new(date, book, self)
+    @rentals << new_rental unless @rentals.include?(new_rental)
   end
 
   private
